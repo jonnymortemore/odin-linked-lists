5fca1ad2798e4b45d6ba1b8f97356fdd6d289084
Jonny Mortemore <jonathanmortemore@gmail.com>
2026-01-16 16:54:05 +1100

    added "contains" functionality

--- classes.js
+++ classes.js
@@ -1,107 +1,115 @@
 export class LinkedList {
-    constructor() {
-        this.headNode = null
+  constructor() {
+    this.headNode = null;
+  }
+
+  append(value) {
+    //add new node to end of list
+    if (this.headNode === null) {
+      this.headNode = new Node(value);
+    } else {
+      findEndToAppend(this.headNode);
     }
 
-    append(value) {
-        //add new node to end of list
-        if (this.headNode === null) {
-            this.headNode = new Node(value);
-        } else {
-            findEndToAppend(this.headNode)
-        }
-
-        function findEndToAppend(node) {
-            if(node.nextNode === null) {
-                node.nextNode = new Node(value);
-                return
-            }
-            findEndToAppend(node.nextNode);
-        };
+    function findEndToAppend(node) {
+      if (node.nextNode === null) {
+        node.nextNode = new Node(value);
+        return;
+      }
+      findEndToAppend(node.nextNode);
     }
+  }
 
-    prepend(value) {
-        //new node containing value to start of list
-        this.headNode = new Node(value, this.headNode);
+  prepend(value) {
+    //new node containing value to start of list
+    this.headNode = new Node(value, this.headNode);
+  }
+
+  size() {
+    //total number of nodes in list
+    return findIndexTotal(this.headNode, 0);
+
+    function findIndexTotal(node, index) {
+      if (node.nextNode === null) {
+        return (index += 1);
+      }
+      return findIndexTotal(node.nextNode, (index += 1));
     }
+  }
 
-    size() {
-        //total number of nodes in list
-        return findIndexTotal(this.headNode, 0);
+  head() {
+    //return value of first node
+    return this.headNode.value;
+  }
 
-        function findIndexTotal(node, index) {
-            if(node.nextNode === null) {
-                return index += 1
-            }
-            return findIndexTotal(node.nextNode, index += 1);
-        };
+  tail() {
+    //return the value of the final node
+    return getFinalNode(this.headNode);
+
+    function getFinalNode(node) {
+      if (node.nextNode === null) {
+        return node.value;
+      }
+      return getFinalNode(node.nextNode);
     }
+  }
 
-    head() {
-        //return value of first node
-        return this.headNode.value
+  at(index) {
+    //value at index. Return undefined if no node at index.
+    return getValueAtIndex(this.headNode, 0);
+
+    function getValueAtIndex(node, curIndex) {
+      if (curIndex === index) {
+        return node.value;
+      }
+      if (node.nextNode === null) {
+        return undefined;
+      }
+
+      return getValueAtIndex(node.nextNode, (curIndex += 1));
     }
+  }
 
-    tail() {
-        //return the value of the final node
-        return getFinalNode(this.headNode);
+  pop() {
+    //remove the head node from the list and return it's value. Undefined on empty.
+    const poppedValue = this.headNode.value;
+    this.headNode = this.headNode.nextNode;
+  }
 
-        function getFinalNode(node) {
-            if(node.nextNode === null) {
-                return node.value
-            }
-            return getFinalNode(node.nextNode)
-        }
+  contains(value) {
+    return checkforValue(this.headNode);
+    function checkforValue(node) {
+      if (node.value === value) {
+        return true;
+      }
+      if (node.value !== value && node.nextNode === null) {
+        return false;
+      }
+      return checkforValue(node.nextNode);
     }
+  }
 
-    at(index) {
-        //value at index. Return undefined if no node at index. 
-        return getValueAtIndex(this.headNode, 0);
+  findIndex(value) {
+    //return the index of the node with the given value. Return -1 if none.
+  }
 
-        function getValueAtIndex(node, curIndex) {
-            if (curIndex === index) {
-                return node.value
-            }
-            if (node.nextNode === null) {
-                return undefined
-            }
+  toString() {
+    //represents your linked list as a string
+    return getNodeStr(this.headNode, "");
 
-            return getValueAtIndex(node.nextNode, curIndex += 1)
-        }
+    function getNodeStr(node, nodeString) {
+      if (node.nextNode === null) {
+        const currentNodeString = nodeString + `(${node.value})`;
+        return currentNodeString;
+      }
+      const currentNodeString = nodeString + `(${node.value}) -> `;
+      return getNodeStr(node.nextNode, currentNodeString);
     }
-
-    pop() {
-        //remove the head node from the list and return it's value. Undefined on empty. 
-        const poppedValue = this.headNode.value
-        this.headNode = this.headNode.nextNode
-    }
-
-    contains(value) {
-        //return true if passed value is in list
-    }
-
-    findIndex(value) {
-        //return the index of the node with the given value. Return -1 if none. 
-    }
-
-    toString() {
-        //represents your linked list as a string
-        return getNodeStr(this.headNode, "");
-
-        function getNodeStr(node, nodeString) {
-            if(node.nextNode === null) {
-                const currentNodeString = nodeString + `(${(node.value)})`
-                return currentNodeString
-            }
-            const currentNodeString = nodeString + `(${node.value}) -> `
-            return getNodeStr(node.nextNode, currentNodeString)
-        }
-    }
- }
+  }
+}
 
 class Node {
-    constructor(value = null, nextNode = null) {
-        this.value = value,
-        this.nextNode = nextNode
-    }
+  constructor(value = null, nextNode = null) {
+    (this.value = value), (this.nextNode = nextNode);
+  }
 }
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {LinkedList} from './classes.js'
+import { LinkedList } from "./classes.js";
 
 const list = new LinkedList();
 
@@ -8,12 +8,14 @@ list.append("parrot");
 list.append("hamster");
 list.append("snake");
 list.append("turtle");
-list.prepend("bear")
-console.log(list.toString())
-console.log(list.size())
-console.log(list.head())
-console.log(list.tail())
-console.log(list.at(2))
-console.log(list.at(12))
-list.pop()
-console.log(list.toString())
+list.prepend("bear");
+console.log(list.toString());
+console.log(list.size());
+console.log(list.head());
+console.log(list.tail());
+console.log(list.at(2));
+console.log(list.at(12));
+list.pop();
+console.log(list.toString());
+console.log(list.contains("dog"));
+console.log(list.contains("banana"));
